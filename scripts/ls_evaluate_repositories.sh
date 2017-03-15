86847fbd8e516f63f1e44fef9ee10f99eb4a7957
Christian Shtarkov <cshtarkov@gmail.com>
2017-03-15 17:42:16 +0000

    Update final script

--- scripts/ls_evaluate_repositories.sh
+++ scripts/ls_evaluate_repositories.sh
@@ -6,13 +6,15 @@
 #
 for repo in `echo */`; do
     cd $repo
-    FIRST_TAG=`git tag --sort version:refname | head -n1`
-    LAST_TAG=`git tag --sort version:refname | tail -n1`
-    printf "\nEvaluating $repo from $FIRST_TAG to $LAST_TAG\n"
+    # FIRST_TAG=`git tag --sort version:refname | head -n1`
+    # LAST_TAG=`git tag --sort version:refname | tail -n1`
+    # printf "\nEvaluating $repo from $FIRST_TAG to $LAST_TAG\n"
     cd ..
-    autobump python -r $repo -f $FIRST_TAG -t $LAST_TAG -e -d -cstdout &> "$repo"mismatches.txt
-    stats.py <(mismatches.sh "$repo"mismatches.txt) <(all_versions.sh "$repo"mismatches.txt) > "$repo"stats.txt
+    # autobump python -r $repo -f $FIRST_TAG -t $LAST_TAG -e -d -cstdout &> "$repo"mismatches.txt
+    # stats.py <(mismatches.sh "$repo"mismatches.txt) <(all_versions.sh "$repo"mismatches.txt) > "$repo"stats.txt
     cd $repo
     cumulative_mismatches.py <(mismatches.sh mismatches.txt) <(tags_with_dates.sh) > intervals.txt
+    grep -e before_stable intervals.txt >> ../before_stable.txt
+    grep -e after_stable intervals.txt >> ../after_stable.txt
     cd ..
 done
